0f9a3a08e49be9378cc5f8cf49c737d31b613da5
Vinsi <40694625+VinsiGit@users.noreply.github.com>
2024-05-22 14:18:24 +0200

    correcte puckscriptTeam

--- Assets/scripts/agents/PuckScriptTeam.cs
+++ Assets/scripts/agents/PuckScriptTeam.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Policies;
 
 public class PuckScriptTeam : MonoBehaviour
 {
@@ -15,35 +17,35 @@ public class PuckScriptTeam : MonoBehaviour
     private HockeyAgentTeam playerAgent; //player agent
     private HockeyAgentTeam enemyAgent; //enemy agent
 
+    private int playerTeamId;
+    private int enemyTeamId;
+
+
     public float reward=1;
 
     void Start () {
+
         playerAgent = player.GetComponent<HockeyAgentTeam>();
         enemyAgent = enemy.GetComponent<HockeyAgentTeam>();
 
     }
     private void OnTriggerEnter(Collider other)
     {
-
         // Check if the entering collider is the player's goal
         if (other.gameObject == playerGoal)
         {
             playerAgent.AddReward(reward); 
             enemyAgent.AddReward(-reward); 
-
-            playerAgent.EndEpisode(); // End the episode for agent1
-            enemyAgent.EndEpisode(); // End the episode for agent1
-
         }
         // Check if the entering collider is the enemy's goal
         else if (other.gameObject == enemyGoal)
         {
-            playerAgent.AddReward(reward); 
-            enemyAgent.AddReward(-reward); 
-
-            playerAgent.EndEpisode(); // End the episode for agent1
-            enemyAgent.EndEpisode(); // End the episode for agent1
-
+            enemyAgent.AddReward(reward); 
+            playerAgent.AddReward(-reward); 
         }
+
+        playerAgent.EndEpisode(); // End the episode for agent1
+        enemyAgent.EndEpisode(); // End the episode for agent1
+
     }
 }
