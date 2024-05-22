753cc43b8300e089b9ac52d1c6e6eb75b78688bf
SolidSalad <grimmieboy17@gmail.com>
2024-05-22 10:52:25 +0200

    npc guardrails

--- Assets/scripts/agents/NPCKeeper.cs
+++ Assets/scripts/agents/NPCKeeper.cs
@@ -8,11 +8,13 @@ public class NPCKeeper : MonoBehaviour
 {
 
     private Rigidbody keeperRb;
+    private Transform keeperTf;
 
     // Start is called before the first frame update
     void Start()
     {
         keeperRb = GetComponent<Rigidbody>();
+        keeperTf = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -22,6 +24,11 @@ public class NPCKeeper : MonoBehaviour
         {
             Vector3 randomForce = new Vector3(Random.Range(-20f, 20f), 0f, Random.Range(-20f, 20f));
             keeperRb.AddForce(randomForce, ForceMode.VelocityChange);
+        }
+
+        if (keeperTf.localPosition.x > -1.0f) // If the agent is on team 0 and has moved to the positive side of the table
+        {
+            keeperTf.localPosition = new Vector3(-1.0f, keeperTf.localPosition.y, keeperTf.localPosition.z); // Reset the x position to 0
         }
     }
 }
