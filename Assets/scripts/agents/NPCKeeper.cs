2ad025e0cebe783be9ec24f5a2b01e3424c52e82
SolidSalad <grimmieboy17@gmail.com>
2024-05-22 10:45:18 +0200

    added npc behavior

--- Assets/scripts/agents/NPCKeeper.cs
+++ Assets/scripts/agents/NPCKeeper.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.MLAgents;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class NPCKeeper : MonoBehaviour
 {
 
-    private Rigidbody agentRb;
-    private Transform agentTf;
+    private Rigidbody keeperRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        keeperRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Random.Range(0f, 60f) <= 1)
+        {
+            Vector3 randomForce = new Vector3(Random.Range(-20f, 20f), 0f, Random.Range(-20f, 20f));
+            keeperRb.AddForce(randomForce, ForceMode.VelocityChange);
+        }
     }
 }
